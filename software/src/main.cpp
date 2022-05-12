01e18194638ae0ed1dd12cfd8464d0f0559a9471
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-05-12 22:14:13 +0100

    move GPS to seperate file

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -160,8 +160,7 @@ int fixstate;  // GPS Fix state-machine. 0=Init, 1=wating for fix,2=fix accuired
 boolean PCConnected;
 uint16_t LoopGPSNoReceiveCount; // If GPS stops working while in ídle mode this will increment
 char LastMaidenHead6[7];        // Holds the Maidenhead position from last transmission, used when GadgetData.WSPRData.TimeSlotCode=17 to determine if the transmitter has moved since last TX
-// The serial connection to the GPS device
-SoftwareSerial GPSSerial(2, 3); // GPS Serial port, RX on pin 2, TX on pin 3
+
 
 // function declarations
 
@@ -2298,57 +2297,6 @@ void PowerSaveON()
 {
     GPSGoToSleep();
     Si5351PowerOff();
-}
-
-void GPSGoToSleep()
-{
-    switch (Product_Model)
-    {
-    case 1017: // Mini
-        // If its the WSPR-TX Mini, send the Sleep string to it
-        GPSSerial.println(F("$PMTK161,0*28"));
-        // GPSSleep = true;
-        break;
-
-    case 1028: // Pico
-        // If it is the WSPR-TX Pico it has a hardware line for sleep/wake
-        pinMode(GPSPower, OUTPUT);
-        digitalWrite(GPSPower, LOW);
-        break;
-    }
-}
-
-void GPSWakeUp()
-{
-    switch (Product_Model)
-    {
-    case 1017: // Mini
-        // Send anything on the GPS serial line to wake it up
-        GPSSerial.println(" ");
-        // GPSSleep = false;
-        delay(100); // Give the GPS some time to wake up and send its serial data back to us
-        break;
-
-    case 1028: // Pico
-        // If it is the WSPR-TX Pico it has a hardware line for sleep/wake
-        pinMode(GPSPower, OUTPUT);
-        digitalWrite(GPSPower, HIGH);
-        delay(200);
-        pinMode(GPSPower, INPUT);
-        delay(200);
-        // Send GPS reset string
-        // GPSSerial.println(F("$PCAS10,3*1F"));
-        // Airborne Mode
-        // GPSSerial.println(F("$PCAS11,5*18"));
-        break;
-    }
-}
-
-void GPSReset()
-{
-    GPSWakeUp();
-    // Send GPS reset string
-    GPSSerial.println(F("$PCAS10,3*1F"));
 }
 
 void Si5351PowerOff()
