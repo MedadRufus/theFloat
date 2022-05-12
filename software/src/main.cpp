dd8e4aae2914ac03655e456f789e264a9fd7bf1b
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-05-12 21:57:14 +0100

    simplify

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -28,6 +28,7 @@
 #include <EEPROM.h>
 #include <SoftwareSerial.h>
 #include <NMEAGPS.h> //NeoGps by SlashDevin"
+#include <defines.hpp>
 
 NMEAGPS gps; // This parses the GPS characters
 gps_fix fix; // This holds on to the latest values
@@ -134,94 +135,12 @@ struct S_FactoryData
 
 // declarations
 
-void DecodeSerialCMD(const char *InputCMD);
-uint8_t BandNumOfHigestLP();
-int GetVCC();
-void NextFreq(void);
-void PickLP(uint8_t TXBand);
-void SendAPIUpdate(uint8_t UpdateType);
-void calcLocator(double lat, double lon);
-boolean CorrectTimeslot();
-
-// Constants
-#define I2C_START 0x08
-#define I2C_START_RPT 0x10
-#define I2C_SLA_W_ACK 0x18
-#define I2C_SLA_R_ACK 0x40
-#define I2C_DATA_ACK 0x28
-#define SI5351A_H
-
-#define SI_CLK0_CONTROL 16 // Register definitions
-#define SI_CLK1_CONTROL 17
-#define SI_CLK2_CONTROL 18
-#define SI_SYNTH_PLL_A 26
-#define SI_SYNTH_PLL_B 34
-#define SI_SYNTH_MS_0 42
-#define SI_SYNTH_MS_1 50
-#define SI_SYNTH_MS_2 58
-#define SI_PLL_RESET 177
-
-#define SI_R_DIV_1 0b00000000 // R-division ratio definitions
-#define SI_R_DIV_2 0b00010000
-#define SI_R_DIV_4 0b00100000
-#define SI_R_DIV_8 0b00110000
-#define SI_R_DIV_16 0b01000000
-#define SI_R_DIV_32 0b01010000
-#define SI_R_DIV_64 0b01100000
-#define SI_R_DIV_128 0b01110000
-
-#define SI_CLK_SRC_PLL_A 0b00000000
-#define SI_CLK_SRC_PLL_B 0b00100000
-
-#define WSPR_SYMBOL_COUNT 162
-
-#define WSPR_FREQ23cm 129650150000ULL // 23cm 1296.501,500MHz (Overtone, not implemented)
-#define WSPR_FREQ70cm 43230150000ULL  // 70cm  432.301,500MHz (Overtone, not implemented)
-#define WSPR_FREQ2m 14449500000ULL    // 2m    144.490,000MHz //Not working. No decode in bench test with WSJT-X decoding Software
-#define WSPR_FREQ4m 7009250000ULL     // 4m     70.092,500MHz //Slightly lower output power
-#define WSPR_FREQ6m 5029450000ULL     // 6m     50.294,500MHz //Slightly lower output power
-#define WSPR_FREQ10m 2812610000ULL    // 10m    28.126,100MHz
-#define WSPR_FREQ12m 2492610000ULL    // 12m    24.926,100MHz
-#define WSPR_FREQ15m 2109610000ULL    // 15m    21.096.100MHz
-#define WSPR_FREQ17m 1810610000ULL    // 17m    18.106,100MHz
-#define WSPR_FREQ20m 1409710000ULL    // 20m    14.097,100MHz
-#define WSPR_FREQ30m 1014020000ULL    // 30m    10.140,200MHz
-#define WSPR_FREQ40m 704010000ULL     // 40m     7.040,100MHz
-#define WSPR_FREQ80m 357010000ULL     // 80m     3.570,100MHz
-#define WSPR_FREQ160m 183810000ULL    // 160m    1.838,100MHz
-#define WSPR_FREQ630m 47570000ULL     // 630m      475.700kHz
-#define WSPR_FREQ2190m 13750000ULL    // 2190m     137.500kHz
-
-#define FactorySpace true
-#define UserSpace false
-
-#define UMesCurrentMode 1
-#define UMesLocator 2
-#define UMesTime 3
-#define UMesGPSLock 4
-#define UMesNoGPSLock 5
-#define UMesFreq 6
-#define UMesTXOn 7
-#define UMesTXOff 8
-#define UMesLPF 9
-#define UMesVCC 10
-#define UMesWSPRBandCycleComplete 11
-
 const uint8_t LP_A = 0;
 const uint8_t LP_B = 1;
 const uint8_t LP_C = 2;
 const uint8_t LP_D = 3;
 
-// Hardware defines
-
 int StatusLED; // LED that indicates current status. Yellow on LP1, Desktop and Mini models, white on Pico
-#define Relay1 5
-#define Relay2 6
-#define Relay3 7
-#define TransmitLED 8 // Red LED next to RF out SMA that will turn on when Transmitting (Pico model do not have a TX LED)
-#define GPSPower A1   // Sleep-Wake signal of the GPS on the WSPR-TX Pico
-#define SiPower A3    // Power the Si5351 from this pin on the WSPR-TX Mini
-
 // Global Variables
 S_GadgetData GadgetData;   // Create a datastructure that holds all relevant data for a WSPR Beacon
 S_FactoryData FactoryData; // Create a datastructure that holds information of the hardware
@@ -239,9 +158,6 @@ uint8_t Si5351I2CAddress;        // The I2C address on the Si5351 as detected on
 uint8_t CurrentBand = 0;         // Keeps track on what band we are currently tranmitting on
 uint8_t CurrentLP = 0;           // Keep track on what Low Pass filter is currently switched in
 const uint8_t SerCMDLength = 50; // Max number of char on a command in the SerialAPI
-void i2cInit();
-uint8_t i2cSendRegister(uint8_t reg, uint8_t data);
-uint8_t i2cReadRegister(uint8_t reg, uint8_t *data);
 
 uint8_t symbolSequence[WSPR_SYMBOL_COUNT];
 uint8_t tx_buffer[WSPR_SYMBOL_COUNT];
@@ -258,6 +174,15 @@ char LastMaidenHead6[7];        // Holds the Maidenhead position from last trans
 SoftwareSerial GPSSerial(2, 3); // GPS Serial port, RX on pin 2, TX on pin 3
 
 // function declarations
+
+void DecodeSerialCMD(const char *InputCMD);
+uint8_t BandNumOfHigestLP();
+int GetVCC();
+void NextFreq(void);
+void PickLP(uint8_t TXBand);
+void SendAPIUpdate(uint8_t UpdateType);
+void calcLocator(double lat, double lon);
+boolean CorrectTimeslot();
 
 // si5351 related
 void si5351aOutputOff(uint8_t clk);
